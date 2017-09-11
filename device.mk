84471ffe6f2875c7d748cbe8137e3a32a2709d20
Marko Man <darkobas@gmail.com>
2017-09-11 11:18:28 +0200

    cheeseburger: build media extensions
    Change-Id: I55f246e168bb763fb89b6ee74f328950591e8a22

--- device.mk
+++ device.mk
@@ -139,6 +139,13 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
 
+# Media Extensions
+PRODUCT_PACKAGES += \
+    libavmediaserviceextensions \
+    mediametrics \
+    libmediametrics \
+    libregistermsext
+
 # ANT+
 #PRODUCT_PACKAGES += \
 #    AntHalService \
