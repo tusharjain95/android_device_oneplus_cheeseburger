62043f31b2585d81de88830d87cb9694e279569e
liquid0624 <rjbirnesser@gmail.com>
2017-09-25 01:29:24 -0400

    cheeseburger: fix gatekeeper service typo

--- hidl.mk
+++ hidl.mk
@@ -85,7 +85,7 @@ PRODUCT_PACKAGES += \
 # Keymaster & Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
-    android.hardware.gatekeeper@1.0-sevice \
+    android.hardware.gatekeeper@1.0-service \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
